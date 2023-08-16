4f1c04f26d817adc8372722c71c806837f67aea3
NKkrisz <nkkrisz2005@gmail.com>
2023-08-16 23:26:14 +0200

    Simple RPS UI And Refactoring

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
+const buttons = document.querySelectorAll("button")
+
+for(const button of buttons){
+    button.addEventListener("click", function () {
+        playRound(button.id)
+    }
+)}
+
 const choices = ["rock", "paper", "scissors"];
-let score = 0;
 
-function getComputerChoice(){
-    return choices[Math.floor(Math.random()*3)]
-}
 
-function game(){
-    
-    const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
+let drawCounter = 0;
 
-    console.log(playerSelection, computerSelection);
+const result = document.querySelector(".result")
+const scores = document.querySelector(".scores")
+const draws = document.querySelector(".draws")
+result.innerText = "Game hasn't started yet"
+scores.innerText = `Player's score: ${playerScore} \n Computer's score: ${computerScore}`
+draws.innerText = `Number of draws: ${drawCounter}`
 
-    if(!choices.includes(playerSelection)){
-        alert("You need to choose rock, paper or scissors!")
+function playRound(playerSelection){
+    const computerSelection = choices[Math.floor(Math.random()*3)];
+    draws.innerText = `Number of draws: ${drawCounter}`
+
+    if(playerSelection === computerSelection){
+        drawCounter++;
+        result.innerText = "It's a draw...";
+        draws.innerText = `Number of draws: ${drawCounter}`
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        result.innerText = "You lose! Paper beats Rock";
+        computerScore++;
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        result.innerText = "You lose! Scissors beats Paper";
+        computerScore++;
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        result.innerText = "You lose! Rock beats Scissors";
+        computerScore++;
     } else {
-        if(playerSelection.toLowerCase() === computerSelection){
-            alert("It's a draw")
-        }
-        else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
-            alert("You lose! Paper beats Rock")
-        }
-        else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
-            alert("You lose! Scissors beats Paper")
-        }
-        else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
-            alert("You lose! Rock beats Scissors")
-        } else {
-            score++;    
-            alert(`You won! ${playerSelection} beats ${computerSelection}`)
-        }
+        result.innerText = `You won! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        playerScore++;
+    }
 
+    scores.innerText = `Player's score: ${playerScore} \n Computer's score: ${computerScore}`
+
+    if(playerScore === 5 || computerScore === 5){
+        playerScore > computerScore ? 
+        result.innerText = " Player won!" :
+        result.innerText = " Computer won!"
+        
+        playerScore = 0;
+        computerScore = 0;
+        drawCounter = 0;
+        return;
     }
 }
 
-let i = 0;
-
-while(i < 5){
-    game();
-    i++;
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
-alert(`You won ${score} out of ${i} tries!`)
